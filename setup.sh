24512c6d406c1abd62a7496e6d1e0eb200f0db81
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2021-05-22 23:24:04 +0300

    hellen

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 mkdir -p ~/.streamlit/
 echo "\
-[general]\n\
-email = \"hellenwain@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
